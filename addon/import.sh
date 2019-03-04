a593c6bf9cf35ebcaeb108061af0ebc63333d38b
genescu <47452713+genescu@users.noreply.github.com>
2019-03-04 04:41:56 +0100

    Update import.sh

--- addon/import.sh
+++ addon/import.sh
@@ -5,6 +5,4 @@
 #localhost:5432
 #database:postgres
 
-#psql -h localhost -U postgres -d postgres -c "\copy  ship  (name,imo,length,port_id,time_started,time_finished) from $SOURCE_PATH delimiter ',' CSV HEADER"
-
-curl -X POST --form "file=@/home/georgeenescu/sftp-creative/ships/ships-demo/src/main/resources/upload/port_visits.csv" http://localhost:8090/upload
+curl -X POST --form "file=@{PATH}/port_visits.csv" http://localhost:8090/upload
